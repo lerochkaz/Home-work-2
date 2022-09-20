5b12ebceba85f06a7f8dc57289d2e17363fbb768
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2022-09-20 23:26:37 +0400

    Внесли изменения. Задача 10

--- Task 10/Program.cs
+++ Task 10/Program.cs
@@ -1,8 +1,15 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.Clear();
-int number = new Random().Next(100, 1000);
-Console.WriteLine(number);
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Неправильный ввод. Попробуйте ещё раз.");
+    return;
+}
+
 int first = number / 100;
 int second = number % 10;
 int result = (number - (first * 100) - second) / 10;
