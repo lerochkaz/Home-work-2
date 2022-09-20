ef9e2c37e1cde219d4f233164d7dea3e1422b469
Zazhiwihina Valeria <zazhiwihina@yandex.ru>
2022-09-20 20:54:37 +0400

    Задача 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите многозначное число (цифры вводить через пробел): ");
+string number = Console.ReadLine() ?? "0";
+string[] dataList = number.Split(" ");
+
+if (dataList.Length > 2) Console.Write(dataList[2]);
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
